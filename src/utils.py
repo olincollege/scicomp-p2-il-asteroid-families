4576d86c6d61c6580d6ae9786039bab3f04acd1d
Ian <ianlum314@gmail.com>
2026-03-10 13:25:57 -0400

    `filter_by_zone` and `load_full` utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Literal
 
 import polars as pl
 
@@ -76,3 +77,56 @@ def load_proper_elements() -> pl.DataFrame:
         ROOT_DIR / "data" / "proper_elements_numbered_and_multiopposition.csv",
         schema=PROPER_ELEMENTS_SCHEMA,
     )
+
+
+def load_full() -> pl.DataFrame:
+    """
+    Returns:
+        pl.DataFrame: DataFrame containing the merged data from membership and proper
+            elements, with missing values filled with zeros.
+    """
+    pes = load_proper_elements()
+    membership = load_membership()
+    full = pes.join(membership, on="name", how="left")
+    full = full.fill_null(strategy="zero")
+    return full
+
+
+def filter_by_zone(
+    df: pl.DataFrame, zone: int, inclination: Literal["all", "low", "high"]
+) -> pl.DataFrame:
+    """
+    Filters the DataFrame by the specified zone as defined by "Milani et al. (2014).
+    Asteroid families classification: Exploiting very large datasets"
+
+    Args:
+        df (pl.DataFrame): The input DataFrame to filter.
+        zone (int): The zone to filter by.
+        inclination (Literal["all", "low", "high"]): The inclination category to filter
+            by. Low will only select asteroids with sinI < 0.3, high will only select
+            asteroids with sinI > 0.3.
+    Returns:
+        pl.DataFrame: A filtered DataFrame containing only rows that match the specified zone.
+    """
+    ZONE_SEMIMAJOR_AXIS_RANGES = {
+        1: (1.600, 2.000),
+        2: (2.000, 2.500),
+        3: (2.500, 2.825),
+        4: (2.825, 3.278),
+        5: (3.278, 3.700),
+        6: (3.700, 4.000),
+    }
+
+    zone_range = ZONE_SEMIMAJOR_AXIS_RANGES[zone]
+    out = df.filter(
+        (pl.col("semimajor_axis") >= zone_range[0])
+        & (pl.col("semimajor_axis") < zone_range[1])
+    )
+
+    match inclination:
+        case "low":
+            out = out.filter(pl.col("sin_inclination") < 0.3)
+        case "high":
+            out = out.filter(pl.col("sin_inclination") >= 0.3)
+
+    return out
